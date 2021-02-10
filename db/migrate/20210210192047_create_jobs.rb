e35d0430435647798a0f9b20b22f0edcbdd928c7
Guilherme A.V. Marques <guilhermevidal4857@gmail.com>
2021-02-10 17:21:24 -0300

    Accessing the jobs basics informations

---
+++ db/migrate/20210210192047_create_jobs.rb
@@ -0,0 +1,15 @@
+class CreateJobs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.text :description
+      t.string :pay_scale
+      t.string :level
+      t.string :requirements
+      t.string :expiration_date
+      t.integer :job_openings
+
+      t.timestamps
+    end
+  end
+end
